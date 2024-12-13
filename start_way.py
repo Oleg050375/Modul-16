f45bbdaeb1ae349c81fc636309ee8c05bf98bdf5
Oleg050375 <ov_varganov@mail.ru>
2024-12-13 06:08:12 +0500

    Add files via upload

--- start_way.py
+++ start_way.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI
 
 srv = FastAPI()  # создание объекта/приложения
 
+
 @srv.get('/')  # хэндлер обработки обращения к главной странице
 async def main_answer() -> str:
-    return ('Главная страница')
+    return 'Главная страница'
+
 
 @srv.get('/user/admin')  # хэндлер обработки обращения к странице администратора
 async def admin_answer() -> str:
-    return ('Вы вошли как администратор')
+    return 'Вы вошли как администратор'
+
 
 @srv.get('/user/{user_id}')  # хэндлер обработки обращения к странице пользователя
-async def user_answer(user_id:int) -> str:
-    return (f'Вы вошли как пользователь № {user_id}')
+async def user_answer(user_id: int) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
+
 
 @srv.get('/user')  # хэндлер обработки запроса данных о пользователе
-async def user_info(username:str, age:int) -> str:
-    return (f'Информация о пользователе. Имя: {username}, Возраст: {age}')
+async def user_info(username: str, age: int) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
